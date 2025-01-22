515cd5465aad338eaf627decda448baaee45ccd6
johnpgr <jp.greidinger@hotmail.com>
2025-01-22 15:50:31 -0300

    Initialize window and renderer

--- src/main.zig
+++ src/main.zig
@@ -4,6 +4,26 @@ const c = @cImport({
     @cInclude("SDL3/SDL.h");
 });
 
+var running = true;
+
+fn handleEvent(e: *c.SDL_Event) void {
+    switch (e.type) {
+        c.SDL_EVENT_QUIT => {
+            running = false;
+        },
+        c.SDL_EVENT_WINDOW_RESIZED => {
+            std.debug.print("Window Resized\n Width: {d}\n  Height: {d}\n", .{ e.window.data1, e.window.data2 });
+        },
+        else => {},
+    }
+}
+
+fn draw(renderer: ?*c.SDL_Renderer) void {
+    _ = c.SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    _ = c.SDL_RenderClear(renderer);
+    _ = c.SDL_RenderPresent(renderer);
+}
+
 pub fn main() !void {
     if (!c.SDL_Init(c.SDL_INIT_VIDEO)) {
         std.debug.print("SDL3 Init Error: {s}\n", .{c.SDL_GetError()});
@@ -11,20 +31,34 @@ pub fn main() !void {
     }
     defer c.SDL_Quit();
 
-    const window = c.SDL_CreateWindow("Zig Made Hero", 800, 600, c.SDL_WINDOW_OPENGL | c.SDL_WINDOW_RESIZABLE);
+    var window: ?*c.SDL_Window = null;
+    var renderer: ?*c.SDL_Renderer = null;
+    _ = c.SDL_CreateWindowAndRenderer(
+        "Zig Made Hero",
+        800,
+        600,
+        c.SDL_WINDOW_OPENGL | c.SDL_WINDOW_RESIZABLE,
+        &window,
+        &renderer,
+    );
+
     if (window == null) {
-        c.SDL_LogError(c.SDL_LOG_CATEGORY_ERROR, "Could not create window: {s}\n", c.SDL_GetError());
+        std.debug.print("Could not create window: {s}\n", .{c.SDL_GetError()});
         return error.SDLWindowCreationFailed;
     }
     defer c.SDL_DestroyWindow(window);
 
-    var quit = false;
-    var event: c.SDL_Event = undefined;
-    while (!quit) {
-        while (c.SDL_PollEvent(&event)) {
-            if (event.type == c.SDL_EVENT_QUIT) {
-                quit = true;
-            }
+    if (renderer == null) {
+        std.debug.print("Could not create renderer: {s}\n", .{c.SDL_GetError()});
+        return error.SDLRendererCreationFailed;
+    }
+    defer c.SDL_DestroyRenderer(renderer);
+
+    var e: c.SDL_Event = undefined;
+    while (running) {
+        while (c.SDL_PollEvent(&e)) {
+            handleEvent(&e);
+            draw(renderer);
         }
     }
 }
